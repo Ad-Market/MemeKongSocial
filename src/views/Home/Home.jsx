5a61d68f1298900fea34e91f02c171e0c31aca7e
Ad-Market <78874562+Ad-Market@users.noreply.github.com>
2022-08-29 13:32:05 +0200

    Update Home.jsx

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -120,13 +120,11 @@ function Home() {
       </Row>
       
       <div style={{marginTop: "50px"}}>
-        <iframe loading="lazy" src="https://app.uniswap.org/#/swap?theme=dark&use=v1?outputCurrency=" style={{border:"0px", margin:"0px auto", display:"block", borderRadius:"20px", maxWidth:"600px"}}
-        id="myId" width="100%" height="600px">
-        </iframe>
+        
       </div>
 
 
-      <div style={{marginTop: "50px"}}><iframe loading="lazy" src="https://teams.bogged.finance/embeds/chart?address=0x29db8abd21f2ffdf5da9b2e4bbdd70f8b47b15f2" style={{border:"0px", margin:"0px auto", display:"block", borderRadius:"20px", maxWidth:"600px"}}
+      <div style={{marginTop: "50px", display:"flex", width:"100%"}}><iframe loading="lazy" src="https://teams.bogged.finance/embeds/chart?address=0x29db8abd21f2ffdf5da9b2e4bbdd70f8b47b15f2" style={{border:"0px", margin:"0px auto", display:"block", borderRadius:"20px", maxWidth:"600px"}}
         id="myId" width="100%" height="600px">
         </iframe>
       </div>
