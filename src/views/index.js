5a5a66238f53c5d8a1b407f08088252573487e01
Ad-Market <78874562+Ad-Market@users.noreply.github.com>
2022-08-29 13:12:17 +0200

    Update index.js

--- src/views/index.js
+++ src/views/index.js
@@ -1,8 +1,5 @@
-// export { default as Home } from "./Home/Home";
-export { default as home } from "./Home/Wallet";
-
-// export { default as Home } from "./Home/Home";
-// export { default as Wallet } from "./Wallet";
+export { default as Home } from "./Home/Home";
+export { default as Wallet } from "./Wallet";
 export { default as Stats } from "./Stats/Stats";
-// export { default as Swap } from "./Swap/Swap";
+export { default as Swap } from "./Swap/Swap";
 export { default as Social } from "./Social/Social";
